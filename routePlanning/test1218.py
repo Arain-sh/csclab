ba5c5452072e3db953d86f16f136fa91a3347954
Arain <liu2596615@163.com>
2018-12-18 16:35:00 +0800

    Update test1218.py

--- routePlanning/test1218.py
+++ routePlanning/test1218.py
@@ -1,9 +1,9 @@
 import sys
 
-fl = open('test.txt','w')
+f = open('./test.txt','w')
 a = sys.argv[1]
 b = sys.argv[2]
 c = sys.argv[3]
 d = sys.argv[4]
-f1.write(a + b + c + d)
-fl.close()
+f.write(a + b + c + d)
+f.close()
